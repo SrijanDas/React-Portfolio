4d800e3c4d3c49e4b15b93f0c6599a14f9c47c00
Srijan Das <srijandas18.sd@gmail.com>
2021-07-31 18:19:37 +0530

    fixed homepage about section

--- src/components/AboutSection/AboutSection.jsx
+++ src/components/AboutSection/AboutSection.jsx
@@ -20,8 +20,8 @@ export default function AboutSection() {
           <PText>
             I am a website designer, developer and machine learning engineer
             from West Bengal, India. I create professional websites and also
-            analyze data and develop machine learning models to help businesses
-            and organisations grow.
+            analyze data and develop machine learning models to solve real world
+            problems.
           </PText>
           <div className="aboutSection__buttons">
             <StyledButton btnLink="/projects" btnText="Works" />
